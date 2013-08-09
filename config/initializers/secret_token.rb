2b2e9fe72698731ed4e2d7fb331b5f0dc47dfa20
Stangcutie723 <Stangcutie723@gmail.com>
2013-08-09 13:32:09 -0700

    Updating in ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoneyDoList::Application.config.secret_key_base = 'd9a3af24501495e0e3bbc6c7187c482062cbe18fefae4a4b55e5ab1238e25fb673d157fa066547fe17387761bd606cfbdff5c8302ea66a86c99d195a9059e6ee'
